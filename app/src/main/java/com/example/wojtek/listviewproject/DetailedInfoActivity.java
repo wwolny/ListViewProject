7cc45e5a1437ec877cff80fc1e8c9f3c0404e90a
wwolny <wwolny1@gmail.com>
2018-10-25 14:57:17 +0900

    DetailedInfoActivity
    Detailed information activity set up

---
+++ app/src/main/java/com/example/wojtek/listviewproject/DetailedInfoActivity.java
@@ -0,0 +1,13 @@
+package com.example.wojtek.listviewproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetailedInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detailed_info);
+    }
+}
